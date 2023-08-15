fb612a76737f029b62abdf8c9c318b7abb2a431a
gobboboy <marcoszgobbo@gmail.com>
2023-08-15 15:30:50 -0300

    Find user by id implementation

--- src/main/java/com/marcosgobbo/workshopmongo/services/UserService.java
+++ src/main/java/com/marcosgobbo/workshopmongo/services/UserService.java
@@ -1,7 +1,9 @@
 package com.marcosgobbo.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.marcosgobbo.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +18,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
---
+++ src/main/java/com/marcosgobbo/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.marcosgobbo.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
