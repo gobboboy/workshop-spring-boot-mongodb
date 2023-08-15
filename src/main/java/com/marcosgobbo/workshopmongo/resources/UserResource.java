8bf3af95e451746574d7ebebbd3c0138e7bdc741
gobboboy <marcoszgobbo@gmail.com>
2023-08-15 17:49:53 -0300

    Endpoint returning user posts

--- src/main/java/com/marcosgobbo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/marcosgobbo/workshopmongo/resources/UserResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.marcosgobbo.workshopmongo.domain.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -54,5 +55,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
