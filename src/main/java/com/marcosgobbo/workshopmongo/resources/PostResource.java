f34ed180b85404fe0de2deb642801f4d127f16c7
gobboboy <marcoszgobbo@gmail.com>
2023-08-15 19:42:00 -0300

    Query with multiple criteria

--- src/main/java/com/marcosgobbo/workshopmongo/resources/PostResource.java
+++ src/main/java/com/marcosgobbo/workshopmongo/resources/PostResource.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.net.URLDecoder;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -39,6 +41,20 @@ public class PostResource {
         text = URL.decodeParam(text);
         List<Post> list = service.findByTitle(text);
 
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/full-search")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "")String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate)
+    {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+
+        List<Post> list = service.fullSearch(text, min, max);
         return ResponseEntity.ok().body(list);
     }
 }
